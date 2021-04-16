83f73f97f0ccb414b92430ceba245dcfde65debc
DChou <dianachou4@gmail.com>
2021-04-15 23:33:14 -0400

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,25 +2,25 @@
 
 # Create movies table
 echo ">>> Creating database..."
-createdb -h localhost a3database
+createdb -U postgres -h localhost a3database
 echo ""
-
+#
 echo ">>> Creating user..."
-createuser -h localhost cmsc828d
+createuser -U postgres -h localhost cmsc828d
 echo ""
 
 echo ">>> Creating tables and granting permissions..."
-psql -h localhost -d a3database -f sql/create_schemas.sql
+psql -U postgres -h localhost -d a3database -f sql/create_schemas.sql
 echo ""
 
 # Load each data file
 echo ">>> Loading data files..."
-psql -d a3database -c "\copy a2.shootings FROM 'data/new_shootings.csv' DELIMITER ',' CSV HEADER;"
-psql -d a3database -c "\copy a3.shootings FROM 'data/new_shootings.csv' DELIMITER ',' CSV HEADER;"
+psql -U postgres -d a3database -c "\copy a2.shootings FROM 'data/new_shootings.csv' DELIMITER ',' CSV HEADER;"
+psql -U postgres -d a3database -c "\copy a3.shootings FROM 'data/new_shootings.csv' DELIMITER ',' CSV HEADER;"
 echo ""
 
 echo ">>> Creating optimizations to the server..."
-psql -h localhost -d a3database -f sql/create_optimize.sql
+psql -U postgres -h localhost -d a3database -f sql/create_optimize.sql
 echo ""
 
 echo "Setup done! Run the server by executing 'python3 server.py'"
